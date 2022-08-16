a02eb51a560b0f29f10b19b7ad28a141981e4c2d
akshaykumarthakurnew <109959999+akshaykumarthakurnew@users.noreply.github.com>
2022-08-16 12:40:45 +0200

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -9,9 +9,9 @@ terraform {
   backend "azurerm" {
     # Using SP method to create the state of the backend
     # Using initial state https://github.com/timoteosoutello/devops-azure-initial-terraform-state
-    resource_group_name  = "RESOURCE_GROUP_NAME"
-    storage_account_name = "STORAGE_ACCOUNT_NAME"
-    container_name       = "CONTAINER_NAME"
+    resource_group_name  = "ak-dev-nl"
+    storage_account_name = "akkitestaccount"
+    container_name       = "terraform"
     # state name that will be created
     key                  = "terraform-aks"
   }
